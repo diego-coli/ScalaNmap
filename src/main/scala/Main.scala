e3654cf493d2558bd413732b2f95213b4bd065b1
floyddc <diegocoli68@gmail.com>
2025-04-14 14:40:25 +0200

    project structure

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+import utils.Logger
+import scanner.*
+
+import scala.concurrent.Future
+
+object Main:
+
+  val defaultIP = "192.168.56"
+  val startRange = 1
+  val endRange = 20
+  val defaultPortsToScan: Seq[Int] = 1 to 65535
+
+  case class defaultConfig(showOpen: Boolean = true, verbose: Boolean = false)
+
+  def main(args: Array[String]) =
+    val (ipArg, flags) = args.partition(!_.startsWith("--"))
+    val ip = ipArg.headOption match
+      case Some(ip) => ip
+      case None     => defaultIP
+
+    Logger.info(s"Starting ScalaNmap on subnet: $ip.$startRange-$endRange")
+
+    val scanning = for
+      hostsUp <- discoverHostsUp(ip)
+      hostsUpPorts <- scanAllHosts(hostsUp)
+    yield()
+
+
+private def discoverHostsUp(ip: String): Future[Seq[String]] = ???
+
+private def scanAllHosts(hostsUp: Seq[String]) = ???
---
+++ src/main/scala/utils/Logger.scala
@@ -0,0 +1,21 @@
+package utils
+
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+object Logger {
+  private val timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+  private def timestamp: String = s"[${LocalTime.now().format(timeFormat)}]"
+
+  private def log(msg: String, colorCode: String): Unit =
+    println(s"$colorCode$timestamp $msg${Console.RESET}")
+
+  def info(msg: String): Unit = log(msg, Console.BLUE)
+  def success(msg: String): Unit = log(msg, Console.GREEN)
+  def warn(msg: String): Unit = log(msg, Console.YELLOW)
+  def error(msg: String): Unit = log(msg, Console.RED)
+}
+
+
+ 
