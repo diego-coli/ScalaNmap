0663c19149f17564f6b2e9be2753e135c9fdbeb7
floyddc <diegocoli68@gmail.com>
2025-04-14 14:54:24 +0200

    HostScanner: ping and case classes

--- src/main/scala/scanner/HostsScanner.scala
+++ src/main/scala/scanner/HostsScanner.scala
@@ -1,3 +1,23 @@
 package scanner
 
- 
+import java.net.InetAddress
+
+sealed trait Result:
+  def ip: String
+
+case class up(ip: String) extends Result
+case class down(ip: String) extends Result
+
+object HostsScanner:
+
+  def ping(ip: String): Option[Result] =
+    InetAddress.getByName(ip) match
+      case _ if InetAddress.getByName(ip).isReachable(500) => Some(up(ip))
+      case _ => Some(down(ip))
+    
+      
+  
+
+
+
+
