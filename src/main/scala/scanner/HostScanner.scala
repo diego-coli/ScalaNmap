4f9c52bfc7932371d44fb35a0ae5409b3044a0d6
floyddc <diegocoli68@gmail.com>
2025-04-19 21:15:52 +0200

    serviceRecon

--- src/main/scala/scanner/HostScanner.scala
+++ src/main/scala/scanner/HostScanner.scala
@@ -12,14 +12,14 @@ case class down(ip: String) extends Result
 
 object HostScanner:
 
-  def pingHost(ip: String): Future[Result] =
+  private def pingHost(ip: String): Future[Result] =
     val timeout = 500
     val address = InetAddress.getByName(ip)
     Future:
       if (address.isReachable(timeout)) up(ip)
       else down(ip)
 
-  def pingRange(netId: String, first: Int, last: Int, config: Config): Future[Seq[Result]] =
+  private def pingRange(netId: String, first: Int, last: Int, config: Config): Future[Seq[Result]] =
     val range = (first to last).map(i => s"$netId.$i")
     val futures = range.map: ip =>
       pingHost(ip).map: result =>
@@ -56,4 +56,4 @@ object HostScanner:
         ResultsManager.printActiveOutOfTotal(hostsUp.size, results.size)
         Future.unit
 
-    def extractActiveHosts(results: Seq[Result]): Seq[String] = results.collect { case up(ip) => ip }
+  private def extractActiveHosts(results: Seq[Result]): Seq[String] = results.collect { case up(ip) => ip }
--- src/main/scala/scanner/PortsScanner.scala
+++ src/main/scala/scanner/PortsScanner.scala
@@ -8,7 +8,7 @@ import scala.util.{Try, Success, Failure}
 
 object PortsScanner:
 
-  val commonPorts: Seq[Int] = Seq(21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 3389)
+  val commonPorts: Seq[Int] = Seq(21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 3306, 3389, 5432, 6379, 8080)
 
   def scanSinglePort(ip: String, port: Int): Future[Option[Int]] =
     Future {
