cdbe2c752675e5da72d410c2d2ea5b70ec7cdfdc
floyddc <diegocoli68@gmail.com>
2025-04-28 18:49:11 +0200

    refactoring and flags changed

--- src/main/scala/utils/ResultsManager.scala
+++ src/main/scala/utils/ResultsManager.scala
@@ -1,60 +1,71 @@
 package utils
-import java.nio.file.{Files, Paths}
+import recon.Services.*
+import java.nio.file.*
 import scanner.*
+import utils.Logger.*
+import java.nio.file.Files.*
+import Paths.*
+import recon.OS.*
 
 object ResultsManager:
 
-  def saveResults(results: Seq[(String, Seq[Int])], includePorts: Boolean = false, includeServices: Boolean = false): Unit =
-    val res = results.map:
-      case (ip, ports) =>
-        if (includePorts && ports.nonEmpty)
-          val portInfo = ports.map: port =>
-            val service = if (includeServices) ServiceRecon.recognize(ip, port) else ""
-            if (service.nonEmpty) s"$port ($service)" else s"$port"
-          .mkString(", ")
-          s"Host: $ip\nOpen ports: $portInfo"
-        else
-          s"Host: $ip"
+  def saveResults(results: Seq[(String, Seq[Int])], config: Config): Unit =
+    if (config.saveOnFile)
+      val res = results.map:
+        case (ip, ports) =>
+          if (config.showOpenPorts && ports.nonEmpty)
+            val portInfo = ports.map: port =>
+              val service = if (config.showServices) recognizeService(ip, port) else ""
+              if (service.nonEmpty) s"$port ($service)" else s"$port"
+            .mkString(", ")
+            s"Host: $ip\nOpen ports: $portInfo"
+          else
+            s"Host: $ip"
 
-    val outputPath = Paths.get("results.txt")
-    Files.write(outputPath, res.mkString("\n").getBytes)
-    Logger.info(s"\nActive hosts${
-      if (includePorts) ", open ports"
-      else ""
-    }${
-      if (includeServices) " and services"
-      else ""
-    } saved in: ${outputPath.toAbsolutePath}")
+      val outputPath = get("results.txt")
+      write(outputPath, res.mkString("\n").getBytes)
+      info(s"\nActive hosts${
+        if (config.showOpenPorts) ", open ports"
+        else ""
+      }${
+        if (config.showServices) " and services"
+        else ""
+      } saved in: ${outputPath.toAbsolutePath}")
 
-  def printActiveHosts(hostsUp: Seq[String]): Unit =
-    if (hostsUp.nonEmpty)
-      Logger.success("\nActive hosts:")
-      hostsUp.foreach(ip => Logger.success(s" - $ip"))
-    else
-      Logger.error("\nNo active hosts found.")
-
-  def printHosts(result: Result, verbose: Boolean = false): Unit =
-    if (verbose)
-      result match
-        case up(ip)   => Logger.success(s"Host UP: $ip")
-        case down(ip) => Logger.warn(s"Host DOWN: $ip")
-
-  def printPorts(ip: String, openPorts: Seq[Int], showOpenPorts: Boolean = false, showServices: Boolean = false): Unit =
-    if (showOpenPorts)
-      if (openPorts.isEmpty) Logger.warn(s"No open ports found on $ip.")
+  def printActiveHosts(hostsUp: Seq[String], config: Config): Unit =
+    if(!config.verboseMode)
+      if (hostsUp.nonEmpty)
+        success("\nActive hosts:")
+        hostsUp.foreach(ip => success(s" - $ip"))
       else
-        Logger.success(s"Open ports on $ip:")
+        error("\nNo active hosts found.")
+
+  def printHosts(result: Result, config: Config, range: Boolean = false): Unit =
+    if (config.verboseMode || !range)
+      result match
+        case up(ip)   => success(s"Host UP: $ip")
+        case down(ip) => warn(s"Host DOWN: $ip")
+
+  def printPorts(ip: String, openPorts: Seq[Int], config: Config): Unit =
+    if (config.showOpenPorts)
+      if (openPorts.isEmpty) warn(s"No open ports found on $ip.")
+      else
+        success(s"Open ports on $ip:")
         openPorts.foreach{
           port =>
-            val service: String = printService(ip, port, showServices)
-            Logger.success(s"$port\t$service")
+            val service: String = printService(ip, port, config)
+            success(s"$port\t$service")
         }
-    else if (!showOpenPorts && openPorts.nonEmpty) Logger.info("Open ports found! Re-run with -O to see them.")
+    else if (!config.showOpenPorts && openPorts.nonEmpty) info("Open ports found! Re-run with -P to see them.")
 
   def printActiveOutOfTotal(active: Int, total: Int): Unit =
     val msg = s"\nFinished: $active active hosts out of $total."
-    if (active > 0) Logger.success(msg) else Logger.error(msg)
+    if (active > 0) success(msg) else error(msg)
 
-  private def printService(ip: String, port: Int, showServices: Boolean = false): String =
-    if (showServices) ServiceRecon.recognize(ip, port)
+  def printOS(hostsUp: Seq[String], config: Config): Unit =
+    if (hostsUp.nonEmpty && config.detectOS)
+      hostsUp.foreach(ip => detectOS(ip))
+
+  private def printService(ip: String, port: Int, config: Config): String =
+    if (config.showServices) recognizeService(ip, port)
     else ""
